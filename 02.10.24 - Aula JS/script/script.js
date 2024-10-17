31ac30febc931d57912ccfffe239eb6a9c5a22c8
AlineNevesdeMelo <160277352+AlineNevesdeMelo@users.noreply.github.com>
2024-10-17 17:48:39 -0300

    Add files via upload

---
+++ 02.10.24 - Aula JS/script/script.js
@@ -0,0 +1,36 @@
+var mensagem="olá, mundo";
+alert(mensagem);
+
+var a=20;
+var b=19;
+/*
+var soma=a+b;
+var sub=a-b;
+var mult=a*b;
+var div=a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+*/
+var qt_i19=5;
+var qt_i20=8;
+var qt_i21=2;
+var qt_i22=1;
+var qt_i24=1;
+var qt_i25=1;
+var qt_i26=1;
+var qt_i41=1;
+var idade19=19*qt_i19;
+var idade20=20*qt_i20;
+var idade21=21*qt_i21;
+var idade22=22*qt_i22;
+var idade24=24*qt_i24;
+var idade25=25*qt_i25;
+var idade26=26*qt_i26;
+var idade41=41*qt_i41;
+
+var media_idades=(idade19+idade20+idade21+idade22+idade24+idade25+idade26+idade41)/20;
+
+alert("A média de idade da sala é: "+media_idades);
